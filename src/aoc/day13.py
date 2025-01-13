5560bbe0d5371bac047e2dcdf985170a37ebd0af
Eric Krämer <kraemer.eric@live.de>
2025-01-13 10:01:37 +0100

    Added day 13 part two

--- src/aoc/day13.py
+++ src/aoc/day13.py
@@ -1,6 +1,5 @@
 import re
 from pathlib import Path
-from typing import Final
 
 import numpy as np
 
@@ -9,7 +8,7 @@ from aoc.common.executor import Executor
 
 INPUT_REGX = re.compile(r"\D+(\d+)\D+(\d+)\n\D+(\d+)\D+(\d+)\n\D+(\d+)\D+(\d+)")
 
-def read_input(file: Path) -> list[tuple]:
+def read_input(file: Path) -> list[tuple[int, ...]]:
     
     data = open(file, "r").read()
 
@@ -20,47 +19,73 @@ def read_input(file: Path) -> list[tuple]:
 
     return arcades
 
+def binary_search(a_vec: np.ndarray, b_vec: np.ndarray, goal: np.ndarray) -> np.ndarray:
+    """
+    Uses binary search to find values for a and b. The algorithm starts with the range of
+    R = [0, goal / (a.x, a.y)]. Each iteration calculates an estimate of b and trims either
+    the upper or lower half of the range R, based on the error function |b.x - b.y|.
+    :param a_vec: a vector
+    :param b_vec: b vector
+    :param goal: goal vector
+    :return: (a presses, b presses) if a solution exists else (0,0)
+    """
+    def error(a_):
+        b_: np.ndarray = (goal - (a_ * a_vec)) / b_vec
+        return abs(b_[0] - b_[1])
 
-def part_one(file: Path) -> int:
+    lower_bound: float = 0
+    upper_bound: float = np.min(goal / a_vec)
+
+    while True:
+
+        m: float = (upper_bound - lower_bound) / 2
+        a: int = int(lower_bound + m)
+        b: np.ndarray = (goal - (a * a_vec)) / b_vec
+
+        if b[0] == b[1]:
+            # found solution
+            return np.array([a, int(b[0].item())])
+
+        if int(lower_bound) == int(upper_bound):
+            # no solution
+            return np.zeros(2)
+
+        m2: float = m / 2
+        error_lower: float = error(lower_bound + m - m2)
+        error_higher: float = error(lower_bound + m + m2)
+
+        # adjust lower or upper bound based on smaller error
+        if error_lower < error_higher:
+            upper_bound = upper_bound - m
+        else:
+            lower_bound = lower_bound + m
+
+def brute_force(file: Path, pt_two: bool = False):
+    """
+    Brute force binary search approach
+    """
     data = read_input(file)
+    data = np.array([i for t in data for i in t], dtype=int).reshape((-1, 6))
+    A = data[:, :2]
+    B = data[:, 2:4]
+    G = data[:, 4:]
 
-    epsilon: Final[float] = 1e-10
+    if pt_two:
+        G += int(1e+13)
 
-    # Solve equation system by using cramer's rule
-    # M     = [ A.X A.Y ]
-    #         [ B.X B.Y ]
-    #
-    # M_hat = [  B.Y -B.X ]
-    #         [ -A.Y  A.X ]
-    #
-    # P     = [ P.X / det(M) ]
-    #         [ P.Y / det(M) ]
-    #
-    # [A, B].T = Ḿ * P
+    presses = np.empty((len(data), 2))
+    for i in range(len(data)):
 
-    # create arrays
-    data = np.array([i for t in data for i in t]).reshape((-1, 6))
-    M = data[:, :4].reshape((-1, 2, 2))
-    P = data[:, 4:]
+        presses[i] = binary_search(A[i], B[i], G[i])
 
-    # calculate solutions
-    P = P / np.linalg.det(M)[:, None]
-    M_hat = M[:,::-1,::-1] * np.array([[1, -1], [-1, 1]])
-    results = (M_hat @ P[:, :, None]).squeeze()
+    result = int(np.sum(presses * np.array([3, 1])).item())
+    return result
 
-    # ignore rounding errors smaller than epsilon
-    # and ignore solutions which are not integers
-    rounded = np.round(results, 0)
-    rounded[np.abs(rounded - results) > epsilon] = 0
-
-    # sum up all valid results
-    tokens = int(np.sum(np.array([3, 1]) @ rounded.T))
-
-    return tokens
-
+def part_one(file: Path):
+    return brute_force(file)
 
 def part_two(file: Path):
-    pass
+    return brute_force(file, True)
 
 def main():
     executor = Executor(
@@ -72,6 +97,8 @@ def main():
 
     executor.test_one(480)
     executor.one("Tokens")
+
+    executor.two("Tokens")
 
 
 if __name__ == "__main__":
